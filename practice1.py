5dcd8cd749a196c62ad23c2baa338d171340d191
David Adekoyejo <davidadekoyejo47@gmail.com>
2023-10-02 04:17:52 +0300

    explanation of function

--- practice1.py
+++ practice1.py
@@ -2,6 +2,7 @@
 user_input = input("input number\n")
 b = int(user_input)
 def show_meow(a):
+    #The function prints meow when the input is an integer that's 3 and above
     if a >= 3:
         print (f"meow")
     elif a < 3:
